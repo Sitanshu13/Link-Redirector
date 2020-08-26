6960af25e3da5e52d22c1eb955504be14ec4f5ef
Sitanshu <69389824+Sitanshu13@users.noreply.github.com>
2020-08-26 10:04:41 +0530

    Add files via upload

---
+++ link_redirector/link_redirector/link_redirector.py
@@ -0,0 +1,120 @@
+import webbrowser
+print("I can redirect you to the following sites:- ")
+print("Google")
+print("Google Maps")
+print("Instagram")
+print("Facebook")
+print("Twitter")
+print("LinkedIn")
+print("Gmail")
+print("Pinterest")
+print("Discord")
+print("Youtube")
+print("Udemy")
+print("Ai playground by CampK12")
+print("Amazon")
+print("Ebay")
+print("Flipkart")
+print("Yahoo")
+print("Bing")
+print("Github")
+print("Wayscript")
+print("Wikipedia")
+print(" ")
+usage = input("Do you want to use this program? ")
+if(usage == "yes"):
+    site_name = input("Which site are you interested in using? ")
+    if(site_name == "google"):
+        webbrowser.open("https://Google.com")
+    elif(site_name == "Google"):
+        webbrowser.open("https://Google.com")
+    elif(site_name == "Instagram"):
+        webbrowser.open("https://www.instagram.com")
+    elif(site_name == "instagram"):
+        webbrowser.open("https://www.instagram.com")
+    elif(site_name == "Facebook"):
+        webbrowser.open("https://www.facebook.com")
+    elif(site_name == "facebook"):
+        webbrowser.open("https://www.facebook.com")
+    elif(site_name == "Pinterest"):
+        webbrowser.open("https://www.pinterest.com/")
+    elif(site_name == "pinterest"):
+        webbrowser.open("https://www.pinterest.com/")
+    elif(site_name == "Discord"):
+        webbrowser.open("https://www.discord.com/")
+    elif(site_name == "discord"):
+        webbrowser.open("https://www.discord.com/")
+    elif(site_name == "Youtube"):
+        webbrowser.open("https://www.youtube.com/")
+    elif(site_name == "youtube"):
+        webbrowser.open("https://www.youtube.com/")
+    elif(site_name == "Udemy"):
+        webbrowser.open("https://www.udemy.com/")
+    elif(site_name == "udemy"):
+        webbrowser.open("https://www.udemy.com/")
+    elif(site_name == "Ai playground"):
+        webbrowser.open("https://theaiplayground.com/")
+    elif(site_name == "Ai playground by CampK12"):
+        webbrowser.open("https://theaiplayground.com/")
+    elif(site_name == "Amazon"):
+        webbrowser.open("https://Amazon.com/")
+    elif(site_name == "amazon"):
+        webbrowser.open("https://Amazon.com/")
+    elif(site_name == "ebay"):
+        webbrowser.open("https://Ebay.com/")
+    elif(site_name == "Ebay"):
+        webbrowser.open("https://Ebay.com/")
+    elif(site_name == "Flipkart"):
+        webbrowser.open("https://Flipkart.com/")
+    elif(site_name == "flipkart"):
+        webbrowser.open("https://Flipkart.com/")
+    elif(site_name == "Yahoo"):
+        webbrowser.open("https://Yahoo.com/")
+    elif(site_name == "yahoo"):
+        webbrowser.open("https://Yahoo.com/")
+    elif(site_name == "Bing"):
+        webbrowser.open("https://Bing.com/")
+    elif(site_name == "bing"):
+        webbrowser.open("https://Bing.com/")
+    elif(site_name == "Github"):
+        webbrowser.open("https://github.com/")
+    elif(site_name == "github"):
+        webbrowser.open("https://github.com/")
+    elif(site_name == "wayscript"):
+        webbrowser.open("https://wayscript.com/")
+    elif(site_name == "Wayscript"):
+        webbrowser.open("https://wayscript.com/")
+    elif(site_name == "Twitter"):
+        webbrowser.open("https://twitter.com/")
+    elif(site_name == "twitter"):
+        webbrowser.open("https://twitter.com/")
+    elif(site_name == "wikipedia"):
+        webbrowser.open("https://wikipedia.com/")
+    elif(site_name == "Wikipedia"):
+        webbrowser.open("https://wikipedia.com/")
+    elif(site_name == "google maps"):
+        webbrowser.open("https://maps.google.com/")
+    elif(site_name == "Google Maps"):
+        webbrowser.open("https://maps.google.com/")
+    elif(site_name == "Google maps"):
+        webbrowser.open("https://maps.google.com/")
+    elif(site_name == "Linkedin"):
+        webbrowser.open("https://linkedin.com/")
+    elif(site_name == "LinkedIn"):
+        webbrowser.open("https://linkedin.com/")
+    elif(site_name == "linkedin"):
+        webbrowser.open("https://linkedin.com/")
+    elif(site_name == "gmail"):
+        webbrowser.open("https://gmail.com/")
+    elif(site_name == "Gmail"):
+        webbrowser.open("https://gmail.com/")
+    elif(site_name == "Yahoo mail"):
+        webbrowser.open("https://mail.yahoo.com/")
+    elif(site_name == "yahoo mail"):
+        webbrowser.open("https://mail.yahoo.com/")
+    elif(site_name == "Yahoo Mail"):
+        webbrowser.open("https://mail.yahoo.com/")
+elif(usage == "no"):
+    print("Ok then")
+    print("Bye")
+    
